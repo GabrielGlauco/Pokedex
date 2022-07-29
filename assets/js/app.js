9e9831dd111864a06ca171bb5aa88987a3e26a96
Gabriel Glauco <gabrielglauco15@gmail.com>
2022-07-29 16:23:30 -0300

    Pokedex v1

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+function fetchpokemon() {
+const url = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+// url dinâmica
+let pokemonpromisses = []
+// array das prommises/json pois fica mais facil manipular os objetos quando estão dentro de arrays
+
+    for (let i = 1; i <= 150 ; i++) {
+         pokemonpromisses.push(fetch(url(i)).then(response =>response.json()))
+// as promisses serão inseridas dinâmicamente no array e aguardarão sua transformação em json      
+    }
+
+    Promise.all(pokemonpromisses)
+/*O .all retornara as promisses do array de uma so vez quando todas estiverem resolvidas e 
+transformadas em json  
+*/  .then((pokemons) => {
+    
+        const formatado = pokemons.reduce((acumulator, pokemon) => {
+            const tipos = pokemon.types.map(elem => elem.type.name)
+            acumulator += `<li class='card ${tipos[0]}'>
+            <img class="card-image" id='${pokemon.id}'alt="${pokemon.name}">
+            <h2 class='card-title'>${pokemon.name}</h2>
+            <p class='card-subtitle'>${tipos.join(' | ')}</p> 
+            </li>`
+            return acumulator
+        }, '') 
+           
+          const ul =  document.querySelector('.pokedex')
+          ul.innerHTML=formatado
+         
+          for (let i = 1; i <= 150; i++) {
+            let imagem = document.getElementById(`${i}`) 
+            if (i < 10) {
+              imagem.setAttribute('src', `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${i}.png`)
+            } else if(i < 100) {
+              imagem.setAttribute('src', `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${i}.png`)
+            } else{
+              imagem.setAttribute('src', `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${i}.png`)
+            }
+          }
+        }).catch(e => console.warn(e))
+// a extensão serve para trabalhar o vetor apos seus elementos ja terem sido transformados 
+// basicamente escrevendo a lista html de forma automatica pelo javascript através do reduce()   
+}
+ 
+
+
+fetchpokemon()   
+    
+ /* .then((pokemon) => {
+            console.log(pokemon)
+          let lista = document.querySelector('.pokedex')
+          let li = document.createElement('li')
+            li.setAttribute('id', pokemon.id)
+            li.innerHTML= `${pokemon.name} ${pokemon.id} `
+            lista.appendChild(li)
+        })*/
